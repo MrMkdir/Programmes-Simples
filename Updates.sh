50dedb6b7a32f49b43871fe1cca3edc1aa65a511
MrRootHendrix <122949261+MrMkdir@users.noreply.github.com>
2023-01-18 03:05:56 +0100

    Update Updates.sh

--- Updates.sh
+++ Updates.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Le fait maison c'est trop bon !
+# chmod +x update.sh
+# open whith your terminal ./update.sh
 
 sudo apt-get update; sudo apt-get dist-upgrade -y; sudo apt-get autoremove --purge -y
